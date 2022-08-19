2a48463b3d3f09d75603c9ce7712c7171820da10
KrapaKarthik09 <Karthik Krapa@KarthikK-CDG.acidaes.org>
2022-08-19 16:51:50 +0530

    Update to nat.tf file

--- eks/nat.tf
+++ eks/nat.tf
@@ -1,9 +1,14 @@
-resource "aws_subnet" "nat_gateway" {
+# resource "aws_subnet" "nat_gateway" {
+#   count = length(var.private_subnets_cidr)
+#   availability_zone = element(var.azs,count.index)
+#   cidr_block = element(var.private_subnets_cidr,count.index)
+#   vpc_id = aws_vpc.kmb_vpc.id
+#   tags = {
+#     "Name" = "NATGateway"
+#   }
+# }
+
+resource "aws_nat_gateway" "nat_gateway" {
   count = length(var.private_subnets_cidr)
-  availability_zone = element(var.azs,count.index)
-  cidr_block = element(var.private_subnets_cidr,count.index)
-  vpc_id = aws_vpc.kmb_vpc.id
-  tags = {
-    "Name" = "NATGateway"
-  }
+  subnet_id = element(var.private_subnets_cidr,count.index)
 }
